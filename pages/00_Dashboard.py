669e56dc160a2b1d387653c2b802dca75bcf16de
jazminhurtado <jazhurgraster@gmail.com>
2025-11-26 06:40:33 -0500

    Update 00_Dashboard.py

--- pages/00_Dashboard.py
+++ pages/00_Dashboard.py
@@ -155,19 +155,6 @@ a {
 </style>
 """, unsafe_allow_html=True)
 
-# --------------------------------------
-# TÍTULO VISUALIZABLE
-# --------------------------------------
-#st.markdown("""
-#<h1 style='
-    #margin-top: 40px;
-    #font-size: 2.7rem;
-    #font-weight: bold;
-    #color: #212529;
-    #text-align: center;
-#'>Estado Situacional de los Planes del SINAPLAN</h1>
-#""", unsafe_allow_html=True)
-
 # Función nueva para obtener datos de PDC por nivel de gobierno
 def get_pdc_nivel_gobierno():
     url = "https://docs.google.com/spreadsheets/d/1bpzY7fYHQrwqjVKvOV0CpypzbJIPaNUQ/export?format=csv&gid=1288416966"
