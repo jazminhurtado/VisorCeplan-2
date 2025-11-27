e9f06e76c467d7ae2a3e654680ec720c421821e0
jazminhurtado <jazhurgraster@gmail.com>
2025-11-27 18:00:45 -0500

    Update 2_Politicas_Nacionales.py

--- pages/2_Politicas_Nacionales.py
+++ pages/2_Politicas_Nacionales.py
@@ -139,7 +139,7 @@ st.markdown("""
 </style>
 
 <div class="titulo-visor">
-   Visor de Políticas Nacionales del Perú
+   📘Visor de Políticas Nacionales del Perú
 </div>
 """, unsafe_allow_html=True)
 
