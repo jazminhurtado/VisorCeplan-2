89c7f9fa4a8890c7598ae34448fe51896d3d8643
jazminhurtado <jazhurgraster@gmail.com>
2025-11-27 21:28:25 -0500

    Update 2_Politicas_Nacionales.py

--- pages/2_Politicas_Nacionales.py
+++ pages/2_Politicas_Nacionales.py
@@ -314,6 +314,8 @@ if seleccion != "-- Selecciona una política --":
             st.info("No se encontraron columnas de OP/Lineamientos en la selección.")
         else:
             ops = [op for op in sub[c["op"]].dropna().unique()]
+
+ops = [op for op in sub[c["op"]].dropna().unique()]
 for op in ops:
     lin_rows = sub[sub[c["op"]] == op]
     lineamientos = list(dict.fromkeys(lin_rows[c["lin"]].tolist()))
