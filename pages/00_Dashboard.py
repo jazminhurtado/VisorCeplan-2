e9bc539a3dc3288cf0fe824bbe86da35a5eeeb58
jazminhurtado <jazhurgraster@gmail.com>
2025-12-02 18:32:01 -0500

    Update 00_Dashboard.py

--- pages/00_Dashboard.py
+++ pages/00_Dashboard.py
@@ -924,10 +924,10 @@ with col2:
 
 
     elif st.session_state.get("hover_poi", False):
-    st.markdown("### Estado POI por Nivel de Gobierno")
-    datos_niveles = get_poi_nivel_gobierno()
-    for nivel, (form, pend) in datos_niveles.items():
-        resumen_grafico(nivel, form, pend)
+        st.markdown("### Estado POI por Nivel de Gobierno")
+        datos_niveles = get_poi_nivel_gobierno()
+        for nivel, (form, pend) in datos_niveles.items():
+            resumen_grafico(nivel, form, pend)
 
 
 
