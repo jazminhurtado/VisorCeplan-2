72667487eb635cf028a446f69f249432a6b2a160
jazminhurtado <jazhurgraster@gmail.com>
2025-12-02 16:32:38 -0500

    Update 00_Dashboard.py

--- pages/00_Dashboard.py
+++ pages/00_Dashboard.py
@@ -750,10 +750,11 @@ def cargar_datos_dashboard():
             headers = [["nivel", "gobierno"], ["total", "pliegos"], ["con", "pei"], ["sin", "pei"]]
             match_val = "total"
             key_con, key_sin = ["con", "pei"], ["sin", "pei"]
-        else:  # POI
-            headers = [["nivel", "gobierno"], ["total", "ues"]]
+        elif tipo == "POI":
+            headers = [["nivel", "gobierno"], ["total", "ues"], ["formulados"], ["pendientes", "sin", "poi"]]
             match_val = "total"
-            key_con, key_sin = ["en", "poi"], ["sin", "poi"]
+            key_con = ["formulados"]
+            key_sin = = ["pendientes", "sin", "poi"]
 
         hdr = _find_header_row(df, headers)
         if hdr < 0: return (0, 0)
