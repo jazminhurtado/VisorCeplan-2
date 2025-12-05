98e42e481a38aa6a8c3e4d54f61ff3dbe784aa3a
jazminhurtado <jazhurgraster@gmail.com>
2025-12-05 15:03:16 -0500

    Update 01_Dashboard_Detalle.py

--- pages/01_Dashboard_Detalle.py
+++ pages/01_Dashboard_Detalle.py
@@ -1,7 +1,5 @@
 import streamlit as st
 import pandas as pd
-import plotly.express as px
-import matplotlib.pyplot as plt
 import requests
 
 # --- CONFIGURACIÓN DE PÁGINA ---
@@ -24,7 +22,7 @@ st.markdown("""
 """, unsafe_allow_html=True)
 
 # --- TÍTULO Y BOTÓN REFRESCAR ---
-st.title("Estado de Instrumentos")
+st.title("Estado de Instrumentos PDC – PEI – POI)")
 refresh = st.button("🔄 Refrescar datos")
 
 # --- URL DE GOOGLE SHEETS (EstadoPDC) ---
@@ -60,7 +58,7 @@ st.session_state.plan = selected_plan
 if selected_plan == "PDC":
     # Normalizar el nombre de columna en caso haya espacios o mayúsculas
     df_pdc.columns = df_pdc.columns.str.strip().str.lower()
-
+    
     if 'nivel_gobierno' in df_pdc.columns:
         niveles = df_pdc['nivel_gobierno'].dropna().unique().tolist()
         niveles.sort()
@@ -72,37 +70,18 @@ if selected_plan == "PDC":
         )
 
         df_filtrado = df_pdc[df_pdc['nivel_gobierno'].isin(seleccion_nivel)]
-
-      # --- GRÁFICO DE BARRAS ---
-        conteo = df_filtrado['nivel_gobierno'].value_counts().reset_index()
-        conteo.columns = ['Nivel de Gobierno', 'Cantidad']
-
-        fig = px.bar(
-            conteo,
-            x='Nivel de Gobierno',
-            y='Cantidad',
-            text='Cantidad',
-            labels={'Cantidad': 'Cantidad', 'Nivel de Gobierno': 'Nivel de Gobierno'},
-            title="Cantidad de entidades por Nivel de Gobierno"
-        )
-        fig.update_traces(textposition='outside')
-        fig.update_layout(xaxis_tickangle=0)
-
-        st.plotly_chart(fig, use_container_width=True)
-        
-
-  
-    
-    
-    
-    
     else:
         st.sidebar.error("⚠️ La columna 'nivel_gobierno' no fue encontrada.")
         df_filtrado = df_pdc.copy()
-
-    # --- VISUALIZACIÓN DE TABLA ---
-    st.subheader("Vista previa de datos - EstadoPDC")
-    st.dataframe(df_filtrado, use_container_width=True)
-
 else:
-    st.warning("🔧 Visualización aún no implementada para este plan.")
+    df_filtrado = df_pdc.copy()
+
+
+
+
+
+
+
+# --- VISUALIZACIÓN DE TABLA ---
+st.subheader("Vista previa de datos - EstadoPDC")
+st.dataframe(df_pdc, use_container_width=True)
