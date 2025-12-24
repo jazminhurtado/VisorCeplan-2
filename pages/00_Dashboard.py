39697b36b29d8399f7706605951a4250b152fb80
jazminhurtado <jazhurgraster@gmail.com>
2025-12-24 10:17:04 -0500

    Update 00_Dashboard.py

--- pages/00_Dashboard.py
+++ pages/00_Dashboard.py
@@ -651,7 +651,7 @@ def render_map(plan: str):
     )
     
 
-    fig_map.update_traces(
+fig_map.update_traces(
         hovertemplate="""<b>📍 %{customdata[0]}</b><br><br>
 📈 <b>Avance:</b> %{customdata[1]}%<br>
 ✅ <b>Formulados:</b> %{customdata[2]}<br>
@@ -660,11 +660,14 @@ def render_map(plan: str):
 <extra></extra>""",
     marker_line_width=1.2,
     marker_line_color="white"
-)        
-fig_map.update_layout(
+        
+    fig_map.update_layout(
     hovermode="closest",
     clickmode="none"   # 🔴 ESTA LÍNEA evita que desaparezca el departamento
-)    
+    )
+)        
+        
+        
         
    
     fig_map.update_geos(fitbounds="locations", visible=False) 
