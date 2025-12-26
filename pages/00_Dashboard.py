0bbb510e2e13360d533ec01f90586a6c2df24b8f
jazminhurtado <jazhurgraster@gmail.com>
2025-12-26 18:13:04 -0500

    Update 00_Dashboard.py

--- pages/00_Dashboard.py
+++ pages/00_Dashboard.py
@@ -843,7 +843,7 @@ poi_e, poi_p = datos["POI"]
 
 hover_pdc = st.session_state.get("hover_pdc", False)
 
-
+# KPIs
 # KPIs
 c1, c2, c3 = st.columns([1, 1, 1], gap="small")
 
@@ -851,7 +851,9 @@ with c1:
     if "hover_pdc" not in st.session_state:
         st.session_state["hover_pdc"] = False
 
+    # Crear un formulario invisible con submit implícito al hacer clic en el div
     with st.form("pdc_kpi_form"):
+        # Estilos para hacer que el div sea clickeable
         st.markdown("""
         <style>
         .clickable-kpi {
@@ -861,14 +863,22 @@ with c1:
         <div class="clickable-kpi" onclick="document.forms['pdc_kpi_form'].submit();">
         """, unsafe_allow_html=True)
 
+        # Mostrar KPI visual
         kpi_card("PDC", pdc_e, pdc_p, "pliegos", "comprende los GN, GR, GL")
 
         st.markdown("</div>", unsafe_allow_html=True)
+
         submitted = st.form_submit_button("")
+
         if submitted:
             st.session_state["hover_pdc"] = True
-            st.session_state["hover_pei"] = False
-            st.session_state["hover_poi"] = False
+            st.session_state["hover_pei"] = False  # Asegura que PEI se apague
+
+
+
+
+
+
 
 with c2:
     if "hover_pei" not in st.session_state:
@@ -887,11 +897,15 @@ with c2:
         kpi_card("PEI", pei_e, pei_p, "pliegos", "comprende los GN, GR, GL")
 
         st.markdown("</div>", unsafe_allow_html=True)
+
         submitted = st.form_submit_button("")
+
         if submitted:
             st.session_state["hover_pei"] = True
-            st.session_state["hover_pdc"] = False
-            st.session_state["hover_poi"] = False
+            st.session_state["hover_pdc"] = False  # Asegura que PDC se apague
+
+
+
 
 with c3:
     if "hover_poi" not in st.session_state:
@@ -910,11 +924,16 @@ with c3:
         kpi_card("POI", poi_e, poi_p, "UEs", "comprende los GN, GR, GL")
 
         st.markdown("</div>", unsafe_allow_html=True)
+
         submitted = st.form_submit_button("")
+
         if submitted:
             st.session_state["hover_poi"] = True
             st.session_state["hover_pei"] = False
             st.session_state["hover_pdc"] = False
+
+
+
 
 
 # Mapa y Gráficos
@@ -969,10 +988,7 @@ with col2:
         datos_niveles = get_poi_nivel_gobierno()
         for nivel, (form, pend) in datos_niveles.items():
             resumen_grafico(nivel, form, pend)
-
-
-
-    
+   
 
     else:
         resumen_grafico("Estado PDC a Nivel Nacional", pdc_e, pdc_p)
