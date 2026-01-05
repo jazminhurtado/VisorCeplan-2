57831674fc5e00063e8b1d28dae246015a1af0f5
jazminhurtado <jazhurgraster@gmail.com>
2026-01-05 15:29:28 -0500

    Update 00_Dashboard.py

--- pages/00_Dashboard.py
+++ pages/00_Dashboard.py
@@ -209,7 +209,8 @@ def get_poi_nivel_gobierno():
     poi_header_idx = None
     for i, row in df.iterrows():
         row_str = " ".join(str(v).lower() for v in row.tolist())
-        if "total ues" in row_str and "poi" in row_str:
+        if "poi" in row_str and "nivel de gobierno" in row_str:
+    
             poi_header_idx = i
             break
 
