5222d258fb44f9a1e4dd3bd946adb94d0204dad5
jazminhurtado <jazhurgraster@gmail.com>
2026-01-16 13:05:01 -0500

    Update 1_PEI_POI_PDC.py

--- pages/1_PEI_POI_PDC.py
+++ pages/1_PEI_POI_PDC.py
@@ -161,7 +161,7 @@ df_pdc_sheet = cargar_excel_google(
 resumen_pdc = construir_tabla(
     df_pdc_sheet,
     filas=slice(1, 4),
-    nombres=["Nivel de Gobierno", "Total Pliegos", "Formulados", "Pendientes"]
+    nombres=["Nivel de Gobierno", "Formulados", "Pendientes", "Total Pliegos"]
 )
 
 
