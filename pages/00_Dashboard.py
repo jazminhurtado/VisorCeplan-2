657bd4bff07bb9dcd058f5b2d278529cce2899c7
jazminhurtado <jazhurgraster@gmail.com>
2026-01-18 19:30:21 -0500

    Update 00_Dashboard.py

--- pages/00_Dashboard.py
+++ pages/00_Dashboard.py
@@ -71,7 +71,7 @@ a {
 # --------------------------------------
 st.markdown("""
 <h1 style='
-    margin-top: -80px;
+    margin-top: -100px;
     font-size: 2.7rem;
     font-weight: bold;
     color: #212529;
